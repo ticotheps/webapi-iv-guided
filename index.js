987894924d38e3b7d0a17befda790e6b98c19e73
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-21 12:55:36 -0400

    Installs dotenv module with yarn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const dotenv = require('dotenv');
 const server = require('./api/server.js');
 
 // make the port dynamic
 const port = process.env.PORT || 4000; // this says read the info from the process.env file
-                                      // and if there is nothing there, use port 4000
+                                      // and if there is nothing there, use port 4000.
+const greeting = process.env.GREETING;
 
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(`\n*** ${greeting} Server Running on http://localhost:${port} ***\n`);
 });
