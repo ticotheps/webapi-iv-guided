9e57a2049b4538b951d1364d097c612a8527e7cc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-21 12:47:41 -0400

    Makes the port dynamic inside index.js file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+// make the port dynamic
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
