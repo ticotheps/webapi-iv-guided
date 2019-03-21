7f95525c8d49ee9aca4159f67cbfed015f3398bc
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-21 12:50:49 -0400

    Adds process.env.PORT from value of 'const port'

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require('./api/server.js');
 
 // make the port dynamic
-const port = 4000;
+const port = process.env.PORT || 4000; // this says read the info from the process.env file
+                                      // and if there is nothing there, use port 4000
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
